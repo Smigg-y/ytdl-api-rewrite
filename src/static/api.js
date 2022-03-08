6c6f47e8038fbb52eb3f7a3f05541d8394b5be8d
Smiggy <itsespresso@gmail.com>
2022-03-08 22:44:10 +0000

    updates

--- src/static/api.js
+++ src/static/api.js
@@ -1,26 +1,163 @@
 function start() {
-    function remove(id) {
-        document.getElementById(id).removeChild('modal-download');
-    }
+    linkvideo = document.getElementById('linkvideo').value;
 
-    function modal(id) {
-        document.getElementById(id).innerHTML = document.getElementById(id).innerHTML + `
-        <div class="modal-dialog" id="modal-download">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" onclick="remove('test')" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" onclick="remove('test')">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>`;
+    if(!linkvideo || linkvideo.length < 20) {
+        return showAlert('Please fill in the URL correctly!', 'alert');
     };
 
-    modal('test');
+    loadingBtn('bootsearch');
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch(`/info?url=${linkvideo}`, requestOptions)
+    .then(response => {
+        if(response.status != 200) {
+            showAlert('Error! Server did not respond correctly :(', 'alert');
+            console.log(response);
+        } else {
+            response.json().then((data) => {
+                if(!data.success) {
+                    if(data.error.includes('410')) { 
+                        return showAlert(('Error! This video is restricted :('), 'alert');
+                    };
+                    return showAlert(('Error! '+ data.error), 'alert');
+                };
+                
+                document.getElementById('form').innerHTML = downloadScreen(data);
+                showBackBtn();
+            }).catch((err) => {
+                console.log(err);
+                showAlert('Error! ' + err);
+            });
+        };
+    })
+    .catch(error => {
+        showAlert('Error! Could not connect to server.');
+        console.log(error);
+    });
 };
+
+function download(urlType) {
+    loadingBtn('divDownload');
+    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    
+    var type = urlType.startsWith('video_') ? 'video' : 'audio';
+    
+    fetch(`/${type}?url=${linkvideo}&best=true`, requestOptions)
+    .then(response => {
+        if(response.status != 200) {
+            showAlert('error: server did not respond correctly :(', 'alert');
+            console.log(response);
+        } else {
+            response.json().then((data) => {
+                if(!data.success) {
+                    return showAlert(('error: ', data.error), 'alert');
+                };
+                console.log(data.file);
+
+                var link = document.createElement('a');
+                link.href = data.file;
+                link.download = (data.file.split('file='))[1];
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+
+                document.getElementById('divDownload').innerHTML = ''
+                document.getElementById('divDownload').innerHTML = `
+                <div class="alert alert-success" role="alert">
+                    <span class="material-icons">done</span>
+                </div>`;
+            }).catch((err) => {
+                console.log(err);
+                showAlert('error!')
+            });
+        };
+    })
+    .catch(error => {
+        showAlert('error - cannot connect to server');
+        console.log(error);
+    });
+};
+
+function loadingBtn(id) {
+    document.getElementById(id).innerHTML = '';
+    document.getElementById(id).innerHTML = `
+    <button class="btn btn-primary" type="button" disabled>
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        Please Wait...
+    </button>`;
+};
+
+function delay(time) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
+
+function showAlert(msg, type = 'error') {
+    document.getElementById(type).innerText = msg;
+    document.getElementById(type).style.display = 'block';
+     
+    delay(8000)
+    .then(() => {
+        document.getElementById(type).innerText = '';
+        document.getElementById(type).style.display = 'none';
+        resetBtnSearch();
+    });
+};
+
+function resetBtnSearch() {
+    document.getElementById('bootsearch').innerHTML = '';
+    document.getElementById('bootsearch').innerHTML = `
+    <p type="button" class="btn btn-lg btn-primary mt-3" onclick="start()">Search <i class="inline-icon-search material-icons">search</i></p>`;
+};
+
+function downloadScreen(data) {
+    return `
+    <p class="h3 text-light">${data.title}</p>
+        <img class="img-fluid rounded mt-2" src="${data.thumb}">
+        <p class="mt-3 text-light text-light">
+            <strong>Duration:</strong> ${sToTime(data.duration)} <strong>Likes:</strong> ${data.likes}
+        </p>
+        
+        <div id="divDownload" class="mt-4">
+            <p class="text-light text-light h5">
+                Download:
+            </p>
+            <p type="button" class="btn btn-lg btn-primary" id="botaovideo" onclick="download('video_${data.videoid}')">Video <i class="inline-icon-search material-icons">smart_display</i></p>
+            <p type="button" class="btn btn-lg btn-primary" id="botaoaudio" onclick="download('audio_${data.videoid}')">Audio <i class="inline-icon-search material-icons">audiotrack</i></p>
+        </div>
+
+        <p type="button" class="btn btn-lg text-light mt-2" id="bootback" onClick="window.location.reload();">Back</p>`;
+};
+
+function sToTime(duration) {   
+    // Hours, minutes and seconds
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+};
+
+function showBackBtn() {
+    document.getElementById("bootback").style.visibility = "visible";
+}
--- src/web.js
+++ src/web.js
@@ -6,11 +6,26 @@ const app = express();
 var serveIndex = require('serve-index');
 const { default: axios } = require("axios");
 
+const COOKIE = "" //"PREF=f4=4000000&tz=America.Sao_Paulo&f6=40000000; VISITOR_INFO1_LIVE=chRD8Jpd3Z4; SID=HAivj5wx8TtnAupfpgtzkeOM5F_BgBp6LMydqEc5yHk_tCpx6RgHHHD1Kqc1lK1YO7l06g.; __Secure-1PSID=HAivj5wx8TtnAupfpgtzkeOM5F_BgBp6LMydqEc5yHk_tCpxxEPeYhngYzHGj5kZu3SGcg.; __Secure-3PSID=HAivj5wx8TtnAupfpgtzkeOM5F_BgBp6LMydqEc5yHk_tCpxbdcQr8j0slHZaLaO6VaNxg.; HSID=A11sZM_GFn3VDr5mr; SSID=ARx4YvPjyMmfple7T; APISID=5m2xsOBtzFBYp5Gb/Ak_Vod1RdKhxB0kJZ; SAPISID=95rzxvKAK7WQg8je/AkEagixeJxMotp9QE; __Secure-1PAPISID=95rzxvKAK7WQg8je/AkEagixeJxMotp9QE; __Secure-3PAPISID=95rzxvKAK7WQg8je/AkEagixeJxMotp9QE; YSC=W8Y4LA4V9AY; LOGIN_INFO=AFmmF2swRgIhANP7alsn_6gDFHHs7Gj7rQjWNM0foNEEwcbQPo3BtNHpAiEA8UTxLe1S4arxR2kmVYvmdmrVOfkQ3VsBMoHzgfCqwi4:QUQ3MjNmeGE2MkpfYTl3eDgxc1lVVlZBRXVSbklZWC1FYlMzc0I4WGNKbXNpV2E5dFh1c1R5RVBMOXFndWNzVmRnTWpfWFJRZE9oNWJzOFpYbTYyZXpaM29hUmgyYXNGNTRRQUlCdmwtZlZQb3dTbkpIemFkc3JaSUlLT1liZnJGTkZ4cTUzbzBwLU5pNlFwTGowS3pLelFqdVZfcHQ2cGJB; CONSISTENCY=AGDxDePD-SKH7AUqq9Bp6aewNuH-wvN-AIDoa603aVITml7RBZecK0RtOWLn9xUEJNvg_yl9XIFdAzdkeZubErgWzx0PJnVFu9HsmrwyVjrWT7oKBnDg-vxmj5eMTsZflMZgCIhlA5mHVCbEiwQ3lFz7; SIDCC=AJi4QfEm1QkHMPGwsFcG3ROH9eBW5ESuKTVcLlI1b6lEiWzVxwrwIJ0iz2AFDXjHp9iC4wL44Q; __Secure-3PSIDCC=AJi4QfFpBlRv4VdfQ0ogagdUMfGyJPlw5_umkiRUp6wJeUpw5fV83m7Ki-jNvvhpewmCs6KlOWI"
+const getRandom = () => { return `${Math.floor(Math.random() * 10000)}` };
+
+const tryhost = async (req) => {
+    tryurl = req.headers.host
+    try {
+        return 'https://' + tryurl;
+    } catch (error) {
+        return 'https://' + tryurl;
+    };
+};
+
 let port = process.env.PORT || 3000;
+
+// only use on localhost;
+//process.env.FFMPEG_PATH = "C:\\Users\\44778\\Desktop\\ffmpeg-2022-03-07-git-e645a1ddb9-essentials_build\\bin\\ffmpeg.exe";
 
 app.set('json spaces', 4);
 app.use(express.static(__dirname + "/"));
-app.use('/public', serveIndex(__dirname + '/public', { icons: true, stylesheet: "src/static/serve.css"}));
+app.use('/public', serveIndex(__dirname + '/public', { icons: true, stylesheet: "src/static/serve.css" }));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/./static/home.html');
@@ -20,10 +35,149 @@ app.get('/api', function(req, res) {
     res.sendFile(__dirname + '/./static/api.html');
 });
 
+app.get('/audio', async function(req, res) {
+    var vurl = req.query.url;
+    
+    if (!vurl || vurl.length < 11) {
+        return res.json({
+            "success": false, "error": "invalid url"
+        });
+    };
+
+    try {
+        const audio = ytdl(vurl, { requestOptions: { headers: { cookie: COOKIE } } });
+        
+        var vinfo = await getVinfo(vurl);
+        var filename = vinfo.videoid ? 'audio_' + vinfo.videoid : 'audio_' + getRandom()
+
+        video.on('error', error => {
+            return res.json({
+                "success": false, "error": error.message
+            });
+        });
+
+        ffmpeg(audio)
+        .withAudioCodec('libmp3lame')
+        .toFormat('mp3')
+        .saveToFile(`${__dirname}/public/${filename}.mp3`)
+        .on('end', () => {
+            tryhost(req)
+            .then(url => {
+                res.json({
+                    "success": true, "file": `${url}/file/?file=${filename}.mp3`
+                });
+            });
+        })
+        .on('error', function(error) {
+            res.json({
+                "success": false, "error": error.message
+            });
+        });
+    } catch (error) {
+        res.json({
+            "success": false, "error": error.message
+        });
+    };
+});
+
+app.get('/video', async function(req, res) {
+    var vurl = req.query.url;
+    var bestQuality = req.query.best;
+
+    if (!vurl && vurl.length < 11) {
+        return res,json({
+            "success": false, "error": "invalid url"
+        });
+    };
+
+    try {
+        var vinfo = await getVinfo(vurl);
+        var filename = vinfo.videoid ? 'video_' + vinfo.videoid : 'video_' + getRandom();
+
+        var vOpts = bestQuality ? { quality: "highest", filter: "audioandvideo", requestOptions: { headers: { cookie: COOKIE } } } :
+            { requestOptions: { headers: { cookie: COOKIE } } };
+        const video = ytdl(vurl, vOpts);
+
+        video.on('error', error => {
+            res.json({
+                "success": false, "error": error.message
+            });
+        });
+
+        video.on('end', () => {
+            tryhost(req)
+            .then(url => {
+                res.json({
+                    "success": true, "file": `${url}/file/?file=${filename}.mp4`
+                });
+            });
+        });
+
+        video.pipe(fs.createWriteStream(`${__dirname}/public/${filename}.mp4`));
+    } catch (error) {
+        res.json({
+            "success": false, "error": error.message
+        });
+    };
+});
+
+app.get('/file', function(req, res) {
+    let filename = req.query.file;
+    let dir = `${__dirname}/public/${filename}`;
+
+    if (!filename || !fs.existsSync(dir)) {
+        return res.json({
+            "success": false, "error": "file not found"
+        });
+    };
+
+    res.download(`${__dirname}/public/${filename}`)
+});
+
+app.get('/info', async function(req, res) {
+    let link = req.query.url;
+
+    if (!link || link.length < 11) {
+        res.json({
+            "success": false, "error": "no url"
+        });
+    };
+
+    data = await getVinfo(link);
+    return res.json(data);
+});
+
+async function getVinfo(url) {
+    try {
+        return await ytdl.getInfo(url)
+        .then((info) => {
+            return {
+                "success": true,
+                "title": info.videoDetails.title,
+                "videoid": info.videoDetails.videoId,
+                "thumb": info.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url,
+                "duration": info.videoDetails.lengthSeconds,
+                "likes": info.videoDetails.likes
+            };
+        })
+        .catch(error => {
+            return {
+                "success": false,
+                "error": error.message
+            };
+        });
+    } catch (error) {
+        return {
+            "success": false,
+            "error": error.message
+        };
+    };
+};
+
 module.exports = async () => new Promise((resolve) => {
     app.listen(port, function() {
         console.log("Listening on port ", port);
-        if(port == 3000) {
+        if (port == 3000) {
             console.log('Running locally at http://localhost:3000');
         };
 
